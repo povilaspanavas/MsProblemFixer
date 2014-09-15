ec605bed4f442b091f0f31626ceb814c32fdf954
Povilas Panavas <povilas.panavas@precisionprinting.co.uk>
2014-09-15 10:40:03 +0100

    It's support update statements if the first column is id key. It need further adjustments

--- MsProblemFixer/Program.cs
+++ MsProblemFixer/Program.cs
@@ -8,7 +8,7 @@ namespace MsProblemFixer
 {
     class Program
     {
-        private static List<string> _commans = new List<string> { "quote" };
+        private static List<string> _commans = new List<string> { "quote", "update" };
 
         public const string NEW_LINE = "\r\n";
         public const string COLUMN_SEPARATOR = "\t";
@@ -25,11 +25,40 @@ namespace MsProblemFixer
             string[] rows;
             if (_commans.Contains(arg1.ToLower()))
             {
-                const string quoteSeparator = "',\r\n'";
-                rows = text.Split(new string[1] { NEW_LINE }, StringSplitOptions.None);
-                var result = string.Format("in{0}({0}'{1}'{0})", NEW_LINE, string.Join<string>(quoteSeparator, rows));
-                Clipboard.SetText(result);
-                return;
+                if (arg1.ToLower().Contains(_commans[0])) //quote
+                {
+                    const string quoteSeparator = "',\r\n'";
+                    rows = text.Split(new string[1] { NEW_LINE }, StringSplitOptions.None);
+                    var result = string.Format("in{0}({0}'{1}'{0})", NEW_LINE, string.Join<string>(quoteSeparator, rows));
+                    Clipboard.SetText(result);
+                    return;
+                }
+                else if (arg1.ToLower().Contains(_commans[1])) //update
+                {
+                    var sql = text.ToString();
+                    var header = sql.Substring(0, sql.IndexOf(NEW_LINE));
+                    var headerSplit = header.Split(new string[1] { COLUMN_SEPARATOR }, StringSplitOptions.None);
+
+                    rows = sql.Substring(sql.IndexOf(NEW_LINE) + 2).Split(new string[1] { NEW_LINE }, StringSplitOptions.None);
+                    var result = new List<string>();
+                    foreach (var row in rows)
+                    {
+                        
+                        var rowSplitted = row.Split(new string[1] { COLUMN_SEPARATOR }, StringSplitOptions.None);
+                        //if (rowSplitted.Length > 1)
+                        var setPart = new List<string>();
+                        for (int i = 0; i < headerSplit.Length; i++)
+                        {
+                            setPart.Add(headerSplit[i] + " = '" + rowSplitted[i] + "'");
+                        }
+                        //var rowValues = string.Join<string>("'", rowSplitted);
+                        var fullRow = string.Format("UPDATE {0} SET {1} WHERE {2} = '{3}'", "table", string.Join(", ", setPart), headerSplit[0], rowSplitted[0]);
+                        fullRow = fullRow.Replace("'NULL'", "NULL");
+                        result.Add(fullRow);
+                    }
+
+                    Clipboard.SetText(string.Join<string>(NEW_LINE, result));
+                }
             }
             else
             {
